d6a53a543504383ae7168ab7ad183e25e4f353f4
Erick Rodriguez <erickro5150@gmail.com>
2025-05-20 15:19:03 -0400

    home page , navbar, and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ExperiencesComponent } from './pages/experiences/experiences.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { EducationComponent } from './pages/education/education.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'experiences', component: ExperiencesComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'education', component: EducationComponent },
+  { path: '**', redirectTo: '' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ExperiencesComponent } from './pages/experiences/experiences.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { EducationComponent } from './pages/education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ExperiencesComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
